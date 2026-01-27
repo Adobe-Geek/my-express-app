9f49b5dacf0153a847bf86a72643b3679a44dcf0
Ania Komarova <anita.komarova@gmail.com>
2026-01-27 07:11:24 +0100

    changed message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Express say watchtower never never never give up</h1>')
+  res.send('<h1>Hello from Express Hey-hello</h1>')
 })
 
 const PORT = 8080
